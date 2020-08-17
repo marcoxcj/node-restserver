fa7df357c7ba48aeb969075f825a9c2b1bb827d0
marco <marco.bryan00@gmail.com>
2020-08-17 14:32:05 -0400

    Usuario y RESTServer

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ process.env.PORT = process.env.PORT || 3000;
 // Entorno
 //==================
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //==================
 // Bse de datos
@@ -17,9 +17,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 let urlDB;
 
 if (process.env.NODE_ENV == 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://test:123@cluster0-ofofh.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = 'mongodb+srv://test:123@cluster0-ofofh.mongodb.net/cafe?retryWrites=true&w=majority';
 };
 
 process.env.URLDB = urlDB;
