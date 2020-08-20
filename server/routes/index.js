ac5ac0f55dc13c098c44b676530a7df4b8c2f0ff
marco <marco.bryan00@gmail.com>
2020-08-20 18:28:56 -0400

    Productos y categorias

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,12 @@ const app = express();
 
 const usuario = require('./usuario');
 const login = require('./login');
+const categoria = require('./categoria');
+const producto = require('./producto');
 
 app.use(usuario);
 app.use(login);
+app.use(categoria);
+app.use(producto);
 
 module.exports = app;
